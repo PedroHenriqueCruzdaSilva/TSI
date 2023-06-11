3614a250b2edb937062014049134cc573beef90c
PedroHenriqueCruzDaSilva <samuraigamer100000@gmail.com>
2023-06-11 16:16:16 -0300

    Helmet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import { GlobalStyle } from "./components/ResetGlobal";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <React.Fragment>
       <GlobalStyle />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Tecnologia Smart e Internet</title>
+      </Helmet>
       <Body />
       <Footer />
     </React.Fragment>
